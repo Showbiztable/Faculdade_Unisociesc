ede21042ea102feb25469eb0e004b693b1a8c357
Showbiztable <40076377+Showbiztable@users.noreply.github.com>
2019-12-01 20:30:14 -0300

    Add files via upload
    Exercícios realizados durante as aulas de sistema.

---
+++ Sistema Operacional 2/Xenomai/Exercícios/ex02b.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <alchemy/task.h>
+
+#define NTASKS 5
+
+RT_TASK task[NTASKS];
+
+// function to be executed by task
+void demo(void *arg)
+{
+	RT_TASK_INFO curtaskinfo;
+
+	printf("Hello World!\n");
+
+	rt_task_inquire(NULL,&curtaskinfo);
+	printf("Task : %s \n", curtaskinfo.name);
+}
+
+int main(int argc, char* argv[])
+{
+	char str[NTASKS][10];
+
+	for(i=1;i<NTASKS;i++){
+		sprintf(str[i], "Thread %i", i);
+		rt_task_create(&task[i], str[i],0,50,0);
+	}
+
+	//Start task
+	//Arguments: &task, task function, function argument
+	for(i=1;i<NTASKS;i++){
+		rt_task_start(&task[i], &demo, 0);
+	}
+}
